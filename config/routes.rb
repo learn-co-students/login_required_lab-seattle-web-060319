0796e4331b12e54af712ff9303ca539a3ac92527
Jason Leach <jlspursfan@gmail.com>
2019-07-10 19:55:43 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#current_user'
+  post '/login' => 'sessions#create'
+  get '/secrets' => 'secrets#show'
+end
 end
