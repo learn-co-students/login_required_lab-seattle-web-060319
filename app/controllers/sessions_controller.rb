7590ce9898f4a4daab446f30b41f21047b0b847a
Jason Leach <jlspursfan@gmail.com>
2019-07-10 20:23:56 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,16 @@
 class SessionsController < ApplicationController
-
   def new
-    redirect_to 'login'
   end
 
   def create
-    if (params[:name] == nil) || (params[:name].empty?)
-      redirect_to '/login'
-    else
-      session[:name] = params[:name]
-      redirect_to '/'
-    end
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
     session.delete :name
+    redirect_to controller: 'application', action: 'hello'
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#current_user'
+  get 'secrets/new'
+
+  root 'application#hello'
+  get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/secrets' => 'secrets#show'
-end
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
 end
